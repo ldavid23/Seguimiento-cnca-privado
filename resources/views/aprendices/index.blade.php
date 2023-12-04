241d5b3b093310382c1abf812ad5aab0a9c2c77b
ldavid23 <ldavidjb05@gmail.com>
2023-12-04 14:59:21 -0500

    crud aprendiz parte 3(Uso mejor de sweetalert sin ajax)

--- resources/views/aprendices/index.blade.php
+++ resources/views/aprendices/index.blade.php
@@ -1,76 +1,81 @@
 @extends('layouts.app')
 @section('css')
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
 @endsection
 
 @section('content')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-15">
-            <h2>Lista de Aprendices</h2>
-            <div class="container">
-                <table id="example" class="table table-light table-striped table-hover table-bordered table-datatable" style="width:100%">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Nombre Completo</th>
-                            <th>N° Documento</th>
-                            <th>N° Ficha</th>
-                            <th>Nombre Ficha</th>
-                            <th>Telefono</th>
-                            <th>Correo</th>
-                            <th>Direccion</th>
-                            <th>Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                            @forelse ($aprendices as $aprendiz)
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-15">
+                <h2>Lista de Aprendices</h2>
+                <div class="container">
+                    <table id="example" class="table table-light table-striped table-hover table-bordered table-datatable"
+                        style="width:100%">
+                        <thead>
                             <tr>
-                                <td>{{$aprendiz->id}}</td>
-                                <td>{{$aprendiz->nombre_aprendiz}}</td>
-                                <td>{{$aprendiz->n_documento}}</td>
-                                <td>{{$aprendiz->n_ficha}}</td>
-                                <td class="align-top">{{$aprendiz->nombre_ficha}}</td>
-                                <td>{{$aprendiz->telefono}}</td>
-                                <td>{{$aprendiz->correo}}</td>
-                                <td>{{$aprendiz->direccion}}</td>
-                                 <td>{{--<button data-bs-toggle="modal" data-bs-target="#UpdateForm{{$aprendiz->id}}" class="btn btn-outline-warning  btn-sm">Editar</button>
+                                <th>ID</th>
+                                <th>Nombre Completo</th>
+                                <th>N° Documento</th>
+                                <th>N° Ficha</th>
+                                <th>Nombre Ficha</th>
+                                <th>Telefono</th>
+                                <th>Correo</th>
+                                <th>Direccion</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($aprendices as $aprendiz)
+                                <tr>
+                                    <td>{{ $aprendiz->id }}</td>
+                                    <td>{{ $aprendiz->nombre_aprendiz }}</td>
+                                    <td>{{ $aprendiz->n_documento }}</td>
+                                    <td>{{ $aprendiz->n_ficha }}</td>
+                                    <td class="align-top">{{ $aprendiz->nombre_ficha }}</td>
+                                    <td>{{ $aprendiz->telefono }}</td>
+                                    <td>{{ $aprendiz->correo }}</td>
+                                    <td>{{ $aprendiz->direccion }}</td>
+                                    <td><button data-bs-toggle="modal" data-bs-target="#UpdateForm{{ $aprendiz->id }}"
+                                            class="btn btn-outline-warning  btn-sm">Editar</button>{{--
                                     <form action="{{route("Aprendiz.destroy",["Aprendiz"=>$aprendiz->id])}}" method="post">
                                         @csrf
                                         @method("DELETE")
                                         <button onclick="return confirm('Seguro que quieres borrar?'); " type="submit" class="btn btn-outline-danger btn-sm"> Borrar </button>
                                     </form> --}}
-                                    <button type="button" onclick="edit({{$aprendiz}},)" >Editar </button>
-                                </td>
-                                {{-- @include("aprendices.edit") --}}
-                            </tr>
-                            <script src="../assets/js/edit.js"></script>
+                                        <button type="button" onclick="edit({{ $aprendiz }},)">Editar </button>
+                                    </td>
+                                    @include('aprendices.edit')
+                                </tr>
+
                             @empty
-                                <tr> <td colspan="9" class="table-active">{{__("no data available")}}</td></tr>
+                                <tr>
+                                    <td colspan="9" class="table-active">{{ __('no data available') }}</td>
+                                </tr>
                             @endforelse
-                    </tbody>
-                </table>
-                <button onclick="create()" class="btn btn-outline-primary">{{__("Register")}}</button>
+                        </tbody>
+                    </table>
+                    <button onclick="create()" class="btn btn-outline-primary">{{ __('Register') }}</button>
+                </div>
+                @if (session('mensaje'))
+                    <div class="alert alert-{{ session('mensaje')['type'] }} alert-dismissible fade show" role="alert">
+                        {{ session('mensaje')['title'] }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="close"></button>
+                    </div>
+                @endif
+                <script src="../assets/js/edit.js"></script>
             </div>
-            @if (session("mensaje"))
-            <div class="alert alert-{{session("mensaje")["type"]}} alert-dismissible fade show" role="alert">
-                    {{session("mensaje")["title"]}}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="close"></button>
-            </div>
-            @endif
         </div>
     </div>
-</div>
-@section('js')
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
-<script>
-new DataTable('#example');
-</script>
-<script>
-    function create() {
-    const formulario = `
-        <form method="post" action="{{url("Aprendiz")}}">
+    @section('js')
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
+        <script>
+            new DataTable('#example');
+        </script>
+        <script>
+            function create() {
+                const formulario = `
+        <form method="post" action="{{ url('Aprendiz') }}">
         @csrf
         <label for="nombre_apendiz">Nombre Completo</label>
         <input type="text" name="nombre_aprendiz" id="nombre_aprendiz" class="swal2-input" required>
@@ -89,13 +94,13 @@ new DataTable('#example');
         <button type="submit" class="btn btn-primary"> Registrar </button>
         </form>
         `;
-    Swal.fire({
-        title: "<h2>Registro de Aprendiz</h2>",
-        icon: "info",
-        html: formulario,
-        showConfirmButton: false,
-      });
-};
-</script>
-@endsection
+                Swal.fire({
+                    title: "<h2>Registro de Aprendiz</h2>",
+                    icon: "info",
+                    html: formulario,
+                    showConfirmButton: false,
+                });
+            };
+        </script>
+    @endsection
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,8 +87,8 @@
   <title>{{ config('app.name', 'SEGUIMIENTO') }}</title>
   <link rel="shortcut icon" type="image/png" href="../assets/images/logos/sena_logo.svg" />
   <link rel="stylesheet" href="../assets/css/styles.min.css" />
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
   @yield('css')
-  <meta name="csrf-token" content="{{csrf_token()}}">
 </head>
 
 <body>
@@ -288,7 +288,7 @@
 <script src="../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
 <script src="../assets/libs/simplebar/dist/simplebar.js"></script>
 <script src="../assets/js/dashboard.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
 @yield('js')
 </body>
 
